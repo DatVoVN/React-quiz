969e4e92f626e38edf851f4b3411bfb55848ec0c
DatVoVN <dat246642@gmail.com>
2024-06-28 04:55:10 +0700

    Handle Status

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import { useEffect } from "react"
+import { act, useEffect, useReducer } from "react"
 import Header from "./Header"
 import Main from "./Main"
+import Loader from "./Loader"
+import Error from "./Error"
+import StarScreen from "./StarScreen"
+const initState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: "loading",
+}
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      }
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      }
+    default:
+      throw new Error("Action Unknown")
+  }
+}
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initState)
   useEffect(() => {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("Error"))
-  })
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }))
+  }, [])
+  const numQuestions = questions.length
   return (
     <div className="app">
       <Header />
       <Main className="main">
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StarScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   )
